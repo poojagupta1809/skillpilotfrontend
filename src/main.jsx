d894105db37cae2994297112a45773fe745b3ca9
poojarani <topoojagupta@gmail.com>
2025-10-26 20:29:26 +0530

    Global token setting for axios call

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+// Set a common header for all request types
+let authorization = 'Bearer ' + sessionStorage.getItem("token");
+
+axios.defaults.headers.common['Authorization'] = authorization; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
