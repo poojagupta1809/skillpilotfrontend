405b3e022bbd7bcc6f38fb67f60ba24012626380
poojarani <topoojagupta@gmail.com>
2025-10-26 20:51:23 +0530

    axios token set globally

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
 
-// Set a common header for all request types
-let authorization = 'Bearer ' + sessionStorage.getItem("token");
 
-axios.defaults.headers.common['Authorization'] = authorization; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
