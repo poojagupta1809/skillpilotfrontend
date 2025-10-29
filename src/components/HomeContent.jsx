3178b5206756e55641f8196fe5c2bf75ed9e88e9
Sushmita Singh <sushmitasingh@Sushmitas-MacBook-Pro.local>
2025-10-29 12:16:04 +0530

    Update HomeContent component

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Typography, Box, Grid, Avatar } from '@mui/material';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Footer from './Footer';
 
 
 // TestimonialCard
